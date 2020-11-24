78392280f2e77ac0465d4a202b4f0f5977cb30ac
Oluwajoba <oluwajobabello@gmail.com>
2020-11-24 23:26:20 +0100

    add script / script.py

---
+++ script.py
@@ -0,0 +1,15 @@
+# Contributions-Importer-For-Github/run_script.py
+import git
+from git_contributions_importer import *
+
+# Your private repo or Bitbucket repo
+repo = git.Repo(
+    "git clone git@bitbucket.org:reputationaire-tests/relative-rankings-chrome-extension.git")
+# Your mock repo
+mock_repo = git.Repo("path/to/your/mock-repo")
+importer = Importer([repo], mock_repo)
+# I use both my personal email and work email here,
+# Since the private repo uses work email, and Github profiles uses
+# my work email
+importer.set_author('oluwajobabello@gmail.com')
+importer.import_repository()
